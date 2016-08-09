0385b8a14c9ed5188fb6709b7108b45b9503b5fa
Antoni Boucher <bouanto@zoho.com>
2016-08-08 23:57:53 -0400

    (cargo-release) generate docs

---
+++ huffman/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["decode","Decode the huffman-encoded `input` using the Huffman `tree`. The decoding ends when `decompressed_size` is reached."]],"struct":[["Tree","A huffman tree."]]});
---
+++ search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex["huffman"] = {"doc":"Huffman is a library to decode huffman-encoded data.","items":[[3,"Tree","huffman","A huffman tree.",null,null],[5,"decode","","Decode the huffman-encoded `input` using the Huffman `tree`.\nThe decoding ends when `decompressed_size` is reached.",null,null],[11,"default","","",0,{"inputs":[],"output":{"name":"tree"}}],[11,"new","","",0,{"inputs":[],"output":{"name":"self"}}],[11,"new_with_value","","",0,{"inputs":[{"name":"u8"}],"output":{"name":"self"}}]],"paths":[[3,"Tree"]]};
+initSearch(searchIndex);
